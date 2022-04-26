5ba770da8b10d7efac3b6a764b8ba9b581653c86
csengekulker <k71608883429@ktch.hu>
2022-04-26 09:52:16 +0200

    labeled radio buttons with answers

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -11,6 +11,13 @@ public class MainFrame extends JFrame {
   JLabel aboutLabel;
   JLabel questionLabel;
 
+  String[] answers = {
+    "jgui",
+    "swing",
+    "window",
+    "jwindow"
+  };
+
   JRadioButton[] options = new JRadioButton[4];
   ButtonGroup bGroup;
   JButton checkButton;
@@ -22,8 +29,10 @@ public class MainFrame extends JFrame {
     this.aboutLabel = new JLabel("Balogh Csenge, 2022.04.26, Szoft_I_N");
     this.questionLabel = new JLabel("Melyik Java GUI könyvtár?");
 
+
+
     for (int i=0; i<4;i++) {
-      this.options[i] = new JRadioButton();
+      this.options[i] = new JRadioButton(answers[i]);
     }
 
     this.bGroup = new ButtonGroup();
