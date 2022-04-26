2ebef8a2eb8d5ecbc83e0e28064d92ecec104ce0
csengekulker <k71608883429@ktch.hu>
2022-04-26 09:00:05 +0200

    initialized components

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,6 +1,18 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
 
 public class MainFrame extends JFrame {
+  JLabel aboutLabel;
+  JLabel questionLabel;
+  // JRadioButton opt_1;
+  // JRadioButton opt_2;
+  // JRadioButton opt_3;
+  // JRadioButton opt_4;
+
+  // helyett tomb :) 
+
+  JRadioButton[] options = new JRadioButton[4];
   public MainFrame() {
 
     this.setTitle("App");
