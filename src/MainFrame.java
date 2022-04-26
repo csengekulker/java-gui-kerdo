e21ad3397a7bcd5b3dcbd03bdfb2a68588c8aab8
csengekulker <k71608883429@ktch.hu>
2022-04-26 09:45:29 +0200

    boxLayout on page axis

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,3 +1,4 @@
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -8,23 +9,11 @@ public class MainFrame extends JFrame {
   JPanel mainPanel;
   JLabel aboutLabel;
   JLabel questionLabel;
-  // JRadioButton opt_1;
-  // JRadioButton opt_2;
-  // JRadioButton opt_3;
-  // JRadioButton opt_4;
-
-  // helyett tomb :) 
 
   JRadioButton[] options = new JRadioButton[4];
-
   JButton checkButton;
 
   public MainFrame() {
-
-    this.setTitle("App");
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setSize(400, 300);
-    this.setVisible(true);
 
     this.mainPanel = new JPanel();
 
@@ -33,15 +22,12 @@ public class MainFrame extends JFrame {
 
     for (int i=0; i<4;i++) {
       this.options[i] = new JRadioButton();
-
     }
 
     this.checkButton = new JButton("Vizsgál");
 
-
     //add components
 
-    this.add(mainPanel);
 
     // add the rest to the JPanel
 
@@ -51,6 +37,18 @@ public class MainFrame extends JFrame {
       this.mainPanel.add(options[i]);
     }
     this.mainPanel.add(checkButton);
+
+
+    this.setTitle("App");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setSize(400, 300);
+    this.setVisible(true);
+
+
+    this.add(mainPanel);
+    this.mainPanel.setLayout(
+      new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS)
+    );
     
   }
 }
